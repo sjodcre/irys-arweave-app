8efa55eb61a48cbb6b6cd2b096c2a99be2fb646d
Your Name <you@example.com>
2024-08-18 15:32:08 +0800

    irys arweave upload

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -2,23 +2,23 @@ import Irys from "@irys/sdk";
 import { NextRequest, NextResponse } from "next/server";
 
 const TOP_UP = '200000000000000000'; // 0.2 MATIC
-const MIN_FUNDS = 0.05;
+const MIN_FUNDS = 0.01;
 
 export async function POST(req: NextRequest) {
     try {
         const data = await req.json()
         const network = "mainnet"
-        const token = "matic"
-        const key = process.env.BNDLR_KEY
-        const irys = new Irys({ network, token, key });
+        const token = "arweave"
+        const key = JSON.parse((process.env.BNDLR_KEY_AR|| '').toString());
+        const irys = new Irys({ network, token, key});
 
         let balance = await irys.getLoadedBalance()
         let readableBalance = irys.utils.fromAtomic(balance).toNumber()
-        console.log(`Your balance is: ${readableBalance.toString()} MATIC`);
+        console.log(`Your balance is: ${readableBalance.toString()} AR`);
 
         if (readableBalance < MIN_FUNDS) {
             console.log("not enough need to fund")
-            const res = await irys.fund(TOP_UP);
+            const res = await irys.fund(irys.utils.toAtomic(0.02));
             console.log("Fund status", res)
         }
         console.log("finished funding")
--- app/api/uploadFile/route.ts
+++ app/api/uploadFile/route.ts
@@ -2,24 +2,24 @@ import { NextResponse, NextRequest } from 'next/server'
 import Irys from "@irys/sdk";
 
 const TOP_UP = '200000000000000000'; // 0.2 MATIC
-const MIN_FUNDS = 0.05
+const MIN_FUNDS = 0.01
 
 export async function POST(req: NextRequest) {
   const body = await req.arrayBuffer()
   const buffer = Buffer.from(body)
 
   const network = "mainnet"
-  const token = "matic"
-  const key = process.env.BNDLR_KEY
+  const token = "arweave"
+  const key = JSON.parse((process.env.BNDLR_KEY_AR|| '').toString());
   const irys = new Irys({ network, token, key });
 
   let balance = await irys.getLoadedBalance()
     let readableBalance = irys.utils.fromAtomic(balance).toNumber()
-    console.log(`Your balance is: ${readableBalance.toString()} MATIC`);
+    console.log(`Your balance is: ${readableBalance.toString()} AR`);
 
     if (readableBalance < MIN_FUNDS) {
         console.log("not enough need to fund")
-        const res = await irys.fund(TOP_UP);
+        const res = await irys.fund(irys.utils.toAtomic(0.02));
         console.log("Fund status", res)
     }
     console.log("finished funding")
